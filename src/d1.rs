8ca071719bc97477a336c6a3c053cd2f33a25d84
andi-makes <26302678+andi-makes@users.noreply.github.com>
2021-10-12 22:14:28 +0200

    Learned something about rust macro_rules!
    I used macro_rules! to trim down the slightly repetitive parts of
    the code like the `mod dX;` statements.
    
    Introduced a constant-generic Day Type. This allows me to address
    the Days programmatically using macros.
    
    I plan to automatically generate the run function's match day
    statement, where the generic Days will come in handy.

--- src/d1.rs
+++ src/d1.rs
@@ -1,3 +1,14 @@
-pub fn run(first: bool) {
-    println!("{}", first);
+use super::{Runnable, Day};
+
+impl Runnable for Day<1> {
+    fn init() -> Self {
+        println!("Init Day 1");
+        Self {}
+    }
+    fn one(&self) {
+        println!("Part 1 Day 1")
+    }
+    fn two(&self) {
+        println!("Part 2 Day 1")
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,15 @@
-mod d1;
-// mod d2;
-// mod d3;
-// mod d4;
-// mod d5;
-// mod d6;
-// mod d7;
-// mod d8;
-// mod d9;
-// mod d10;
-// mod d11;
-// mod d12;
-// mod d13;
-// mod d14;
-// mod d15;
-// mod d16;
-// mod d17;
-// mod d18;
-// mod d19;
-// mod d20;
-// mod d21;
-// mod d22;
-// mod d23;
-// mod d24;
-// mod d25;
+macro_rules! include_days {
+    ($($day:ident),+) => {
+        $(
+            mod $day;
+        )+
+    }
+}
+
+include_days!(
+    d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16, d17, d18, d19, d20, d21,
+    d22, d23, d24, d25
+);
 
 pub enum Run {
     Both,
@@ -30,7 +17,60 @@ pub enum Run {
     Two,
 }
 
+macro_rules! run_day {
+    ($d:literal, $run:expr) => {{
+        let day = Day::<$d>::init();
+        day.run($run);
+    }};
+}
+
+struct Day<const DAY: u8>;
+
+trait Runnable {
+    fn init() -> Self;
+    fn one(&self);
+    fn two(&self);
+    fn run(&self, run: Run) {
+        match run {
+            Run::Both => {
+                self.one();
+                self.two();
+            }
+            Run::One => self.one(),
+            Run::Two => self.two(),
+        };
+    }
+}
+
 pub fn run(day: u8, run: Run) {
     println!("Running Day {}", day);
-    d1::run(true);
+
+    match day {
+        1 => run_day!(1, run),
+        // 2 => run_day!(D2, run),
+        // 3 => run_day!(D3, run),
+        // 4 => run_day!(D4, run),
+        // 5 => run_day!(D5, run),
+        // 6 => run_day!(D6, run),
+        // 7 => run_day!(D7, run),
+        // 8 => run_day!(D8, run),
+        // 9 => run_day!(D9, run),
+        // 10 => run_day!(D10, run),
+        // 11 => run_day!(D11, run),
+        // 12 => run_day!(D12, run),
+        // 13 => run_day!(D13, run),
+        // 14 => run_day!(D14, run),
+        // 15 => run_day!(D15, run),
+        // 16 => run_day!(D16, run),
+        // 17 => run_day!(D17, run),
+        // 18 => run_day!(D18, run),
+        // 19 => run_day!(D19, run),
+        // 20 => run_day!(D20, run),
+        // 21 => run_day!(D21, run),
+        // 22 => run_day!(D22, run),
+        // 23 => run_day!(D23, run),
+        // 24 => run_day!(D24, run),
+        // 25 => run_day!(D25, run),
+        _ => panic!("Days out of Bounds! No presents for you!"),
+    }
 }
