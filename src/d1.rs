c93f072963d9b0452a7be70c60fbbfe5ff1915c3
andi-makes <26302678+andi-makes@users.noreply.github.com>
2021-12-01 06:44:54 +0100

    Solved Day 1

--- src/d1.rs
+++ src/d1.rs
@@ -4,19 +4,33 @@ use const_format::formatcp;
 const CURRENT_DAY: u8 = 1;
 const FILE: &'static str = formatcp!("./inputs/input{}.txt", CURRENT_DAY);
 
-type Data = Vec<String>;
+type Data = Vec<u32>;
 impl Runnable<Data> for Day<CURRENT_DAY> {
     fn init() -> (Self, Data) {
         let v: Data = std::fs::read_to_string(FILE)
             .unwrap()
             .lines()
-            .map(|s| s.to_string())
+            .map(|s| s.parse::<u32>().unwrap())
             .collect();
 
         (Self {}, v)
     }
-    fn one(&self, _data: &mut Data) {}
-    fn two(&self, _data: &mut Data) {}
+    fn one(&self, data: &mut Data) {
+        let mut incresed = 0;
+        for i in 1..data.len() {
+            if data[i] > data[i-1] {
+                incresed += 1;
+            }
+        }
+        println!("[{}]", incresed);
+    }
+    fn two(&self, data: &mut Data) {
+        let mut incresed = 0;
+        for i in 3..(data.len()) {
+            if (data[i] + data[i-1] + data[i-2]) > (data[i-1] + data[i-2] + data[i-3]) {
+                incresed += 1;
+            }
+        }
+        println!("[{}]", incresed); // 1851, 1852
+    }
 }
-
-
