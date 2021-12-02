ea8fb055a13dd6d43deb11ff9786128925ae6cb4
andi-makes <26302678+andi-makes@users.noreply.github.com>
2021-12-02 07:30:44 +0100

    Solved day 2

--- src/d2.rs
+++ src/d2.rs
@@ -4,19 +4,81 @@ use const_format::formatcp;
 const CURRENT_DAY: u8 = 2;
 const FILE: &'static str = formatcp!("./inputs/input{}.txt", CURRENT_DAY);
 
-type Data = Vec<String>;
+#[derive(Clone, Debug)]
+pub enum Direction {
+    Forward,
+    Up,
+    Down,
+}
+
+#[derive(Clone, Debug)]
+pub struct DataPoint {
+    dir: Direction,
+    amount: i64,
+}
+
+impl From<String> for DataPoint {
+    fn from(s: String) -> Self {
+        if s.is_empty() {
+            return DataPoint{dir: Direction::Forward, amount: 0};
+        } 
+        let mut s = s.split(' ');
+        if s.clone().count() != 2 {
+            return DataPoint{dir: Direction::Forward, amount: 0};
+        }
+        let dir = match s.next().unwrap() {
+            "forward" => Direction::Forward,
+            "down" => Direction::Down,
+            "up" => Direction::Up,
+            _ => return DataPoint{dir: Direction::Forward, amount: 0},
+        };
+        let amount = s.next().unwrap().parse().unwrap();
+
+        DataPoint{dir, amount}
+    }
+}
+
+type Data = Vec<DataPoint>;
 impl Runnable<Data> for Day<CURRENT_DAY> {
     fn init() -> (Self, Data) {
         let v: Data = std::fs::read_to_string(FILE)
             .unwrap()
             .lines()
-            .map(|s| s.to_string())
+            .map(|s| {DataPoint::from(s.to_string())})
             .collect();
 
         (Self {}, v)
     }
-    fn one(&self, _data: &mut Data) {}
-    fn two(&self, _data: &mut Data) {}
+    fn one(&self, data: &mut Data) {
+        let mut horizontal = 0;
+        let mut vertical = 0;
+
+        for point in data {
+            match point.dir {
+                Direction::Forward => horizontal += point.amount,
+                Direction::Up => vertical -= point.amount,
+                Direction::Down => vertical += point.amount,
+            }
+        }
+        println!("[{}]", horizontal*vertical);
+    }
+    fn two(&self, data: &mut Data) {
+        let mut horizontal = 0;
+        let mut vertical = 0;
+        let mut aim = 0;
+
+        for point in data {
+            match point.dir {
+                Direction::Forward => {
+                    horizontal += point.amount;
+                    vertical += point.amount * aim;
+                },
+                Direction::Up => aim-=point.amount,
+                Direction::Down => aim+=point.amount,
+            }
+        }
+        println!("[{}]", horizontal*vertical); // 385593268
+    }
 }
 
 
