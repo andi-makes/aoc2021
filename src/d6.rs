f513d49efe1d835c822f7c1a5b04326006483482
andi-makes <26302678+andi-makes@users.noreply.github.com>
2021-12-06 07:01:06 +0100

    Faster Day 6

--- src/d6.rs
+++ src/d6.rs
@@ -4,6 +4,36 @@ use const_format::formatcp;
 const CURRENT_DAY: u8 = 6;
 const FILE: &'static str = formatcp!("./inputs/input{}.txt", CURRENT_DAY);
 
+fn evolve(d: &mut [u64; 9], steps: u64) -> u64 {
+    for _ in 0..steps {
+        let mut buffer = [0; 9];
+
+        buffer[8] = d[0];
+        buffer[7] = d[8];
+        buffer[6] = d[7] + d[0];
+        buffer[5] = d[6];
+        buffer[4] = d[5];
+        buffer[3] = d[4];
+        buffer[2] = d[3];
+        buffer[1] = d[2];
+        buffer[0] = d[1];
+
+        // for i in 0..9 {
+        //     match i {
+        //         8 => buffer[8] = d[0],
+        //         6 => buffer[6] = d[7] + d[0],
+        //         _ => buffer[i] = d[i+1],
+        //     }
+        // }
+
+        for i in 0..9 {
+            d[i] = buffer[i];
+        }
+    }
+
+    d.iter().fold(0, |sum, f| sum + f)
+}
+
 type Data = Vec<u64>;
 impl Runnable<Data> for Day<CURRENT_DAY> {
     fn init() -> (Self, Data) {
@@ -17,46 +47,17 @@ impl Runnable<Data> for Day<CURRENT_DAY> {
         (Self {}, v)
     }
     fn one(&self, data: &mut Data) -> u64 {
-        for _ in 0..80 {
-            let mut new = Data::new();
-            for d in &mut *data {
-                if *d == 0 {
-                    *d = 6;
-                    new.push(8);
-                } else {
-                    *d -= 1;
-                }
-            }
-            data.append(&mut new);
-        }
-
-        data.len() as u64
-    }
-    fn two(&self, data: &mut Data) -> u64 { 
         let mut d = [0; 9]; // 0, 1, 2, 3, 4, 5, 6, 7, 8
         for entry in data {
             d[*entry as usize] += 1;
         }
-        for _ in 0..256 {
-            let mut buffer = [0; 9];
-            
-            buffer[8] = d[0];
-            buffer[7] = d[8];
-            buffer[6] = d[7] + d[0];
-            buffer[5] = d[6];
-            buffer[4] = d[5];
-            buffer[3] = d[4];
-            buffer[2] = d[3];
-            buffer[1] = d[2];
-            buffer[0] = d[1];
-
-            for i in 0..9 {
-                d[i] = buffer[i];
-            }
+        evolve(&mut d, 80)
+    }
+    fn two(&self, data: &mut Data) -> u64 {
+        let mut d = [0; 9]; // 0, 1, 2, 3, 4, 5, 6, 7, 8
+        for entry in data {
+            d[*entry as usize] += 1;
         }
-
-        d.iter().fold(0, |sum, f| sum + f)
-     }
+        evolve(&mut d, 256)
+    }
 }
-
-
