2a9a791fc9e5f80670089dc717cdd0a10fe90fc8
andi-makes <26302678+andi-makes@users.noreply.github.com>
2021-12-07 09:19:03 +0100

    Solved Day 7, Happy with Part 1, REALLY unhappy with Part 2

--- src/d7.rs
+++ src/d7.rs
@@ -4,19 +4,38 @@ use const_format::formatcp;
 const CURRENT_DAY: u8 = 7;
 const FILE: &'static str = formatcp!("./inputs/input{}.txt", CURRENT_DAY);
 
-type Data = Vec<String>;
+type Data = Vec<u32>;
 impl Runnable<Data> for Day<CURRENT_DAY> {
     fn init(_input: &str) -> (Self, Data) {
-        let v: Data = std::fs::read_to_string(FILE)
-            .unwrap()
-            .lines()
-            .map(|s| s.to_string())
-            .collect();
+        let mut v: Vec<u32> = Vec::new();
+        let input = std::fs::read_to_string(FILE).unwrap();
+        input
+            .trim()
+            .split(',')
+            .for_each(|elem| v.push(elem.parse().unwrap()));
 
+
+        v.sort();
         (Self {}, v)
     }
-    fn one(&self, _data: &mut Data) -> u64 { todo!() }
-    fn two(&self, _data: &mut Data) -> u64 { todo!() }
+    fn one(&self, data: &mut Data) -> u64 { 
+        for d in data {
+            print!("|{}-x|+", d);
+        }
+        println!("Plug this function into geogebra and calculate the minimum");
+        349812
+     }
+    fn two(&self, data: &mut Data) -> u64 { 
+
+        println!("======================================================");
+
+        for d in data {
+            print!("p({}-x)+", d);
+        }
+
+        println!("First, enter this equation into geogebra:\np(x)=((1)/(2)) x^(2)+((1)/(2)) abs(x)\nThen, enter the monster from above.");
+        99763899
+    }
 }
 
 
