372a1c1b2056d7c8f75a07e25165bca456ac0f9f
andi-makes <26302678+andi-makes@users.noreply.github.com>
2021-12-09 06:29:28 +0100

    Solved Day 9 Part 1

--- src/d9.rs
+++ src/d9.rs
@@ -4,12 +4,41 @@ use const_format::formatcp;
 const CURRENT_DAY: u8 = 9;
 const FILE: &'static str = formatcp!("./inputs/input{}test.txt", CURRENT_DAY);
 
-const WIDTH: u8 = 10;
-const HEIGHT: u8 = 5;
+const WIDTH: usize = 10;
+const HEIGHT: usize = 5;
 
-const SIZE: u8 = WIDTH * HEIGHT;
+// const WIDTH: usize = 100;
+// const HEIGHT: usize = 100;
+
+const SIZE: usize = WIDTH * HEIGHT;
 
 type Data = [u8; SIZE as usize];
+
+fn is_lowest(data: &Data, x: usize, y: usize) -> bool {
+    let mut lowest: bool; // RAII is unhappy now :(
+    if x == 0 {
+        lowest = data[(y * WIDTH) as usize] < data[(y * WIDTH + 1) as usize];
+    } else if x == WIDTH - 1 {
+        lowest = data[(y * WIDTH + x) as usize] < data[(y * WIDTH + x - 1) as usize];
+    } else {
+        lowest = data[(y * WIDTH + x) as usize] < data[(y * WIDTH + x + 1) as usize]
+            && data[(y * WIDTH + x) as usize] < data[(y * WIDTH + x - 1) as usize];
+    }
+    if !lowest {
+        return false;
+    }
+    if y == 0 {
+        lowest = lowest && data[(x) as usize] < data[(WIDTH + x) as usize];
+    } else if y == HEIGHT - 1 {
+        lowest = lowest && data[(y * WIDTH + x) as usize] < data[((y - 1) * WIDTH + x) as usize];
+    } else {
+        lowest = lowest
+            && data[(y * WIDTH + x) as usize] < data[((y + 1)*WIDTH + x) as usize]
+            && data[(y * WIDTH + x) as usize] < data[((y - 1) * WIDTH + x) as usize];
+    }
+    lowest
+}
+
 impl Runnable<Data> for Day<CURRENT_DAY> {
     fn init(_input: &str) -> (Self, Data) {
         let mut v: Data = [0; SIZE as usize];
@@ -29,8 +58,18 @@ impl Runnable<Data> for Day<CURRENT_DAY> {
 
         (Self {}, v)
     }
-    fn one(&self, _data: &mut Data) -> u64 {
-        todo!()
+    fn one(&self, data: &mut Data) -> u64 {
+        let mut sum:u64 = 0;
+        for y in 0..HEIGHT {
+            for x in 0..WIDTH {
+                let low = is_lowest(&data, x, y);
+                println!("[{}|{}]: {} -> {}", x, y, data[(y*WIDTH+x) as usize], low);
+                if low {
+                    sum += data[(y*WIDTH+x) as usize] as u64 + 1;
+                }
+            }
+        }
+        sum
     }
     fn two(&self, _data: &mut Data) -> u64 {
         todo!()
