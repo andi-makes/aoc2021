9b48b01e99d17fddf4e30f07179ccd57f94ffd09
andi-makes <26302678+andi-makes@users.noreply.github.com>
2021-12-13 07:45:39 +0100

    Solved Day 13

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 [![About](https://img.shields.io/badge/Advent%20of%20Code-2021-brightgreen)](https://adventofcode.com/2021/about)
 ![Language](https://img.shields.io/badge/Language-rust-orange)
-![Days completed](https://img.shields.io/badge/Days%20completed-12-red)
-![Stars](https://img.shields.io/badge/Stars-24-yellow)
+![Days completed](https://img.shields.io/badge/Days%20completed-13-red)
+![Stars](https://img.shields.io/badge/Stars-26-yellow)
 
 Advent of Code 2021 - My solutions using rust
 
--- src/d13.rs
+++ src/d13.rs
@@ -1,22 +1,139 @@
+use std::{collections::HashMap, convert::TryInto};
+
 use crate::{Day, Runnable};
 use const_format::formatcp;
 
 const CURRENT_DAY: u8 = 13;
 const FILE: &'static str = formatcp!("./inputs/input{}.txt", CURRENT_DAY);
 
-type Data = Vec<String>;
+type Point = (usize, usize);
+
+#[derive(Clone, Debug)]
+pub struct Fold {
+    along_x: bool,
+    number: usize,
+}
+
+type Data = (HashMap<Point, u8>, Vec<Fold>);
 impl Runnable<Data> for Day<CURRENT_DAY> {
     fn init(_input: &str) -> (Self, Data) {
-        let v: Data = std::fs::read_to_string(FILE)
+        let mut v: Data = (HashMap::new(), Vec::new());
+        std::fs::read_to_string(FILE)
             .unwrap()
             .lines()
-            .map(|s| s.to_string())
-            .collect();
+            .for_each(|s| {
+                if s.contains("fold along") {
+                    if s.contains("x") {
+                        v.1.push(Fold {
+                            along_x: true,
+                            number: s.split('=').last().unwrap().parse().unwrap(),
+                        });
+                    } else {
+                        v.1.push(Fold {
+                            along_x: false,
+                            number: s.split('=').last().unwrap().parse().unwrap(),
+                        });
+                    }
+                } else if s.contains(",") {
+                    let split: Vec<&str> = s.split(',').collect();
+                    v.0.entry((split[0].parse().unwrap(), split[1].parse().unwrap()))
+                        .or_insert(1);
+                    // v.0.push(Point{
+                    //     x: split[0].parse().unwrap(),
+                    //     y: split[1].parse().unwrap(),
+                    // });
+                }
+            });
 
         (Self {}, v)
     }
-    fn one(&self, _data: &mut Data) -> u64 { todo!() }
-    fn two(&self, _data: &mut Data) -> u64 { todo!() }
+    fn one(&self, data: &mut Data) -> u64 {
+        let instruction = &data.1.first().unwrap();
+        if instruction.along_x {
+            for entry in data.0.clone() {
+                if entry.0 .0 > instruction.number {
+                    // *entry.1 = 0;
+                    *data.0.entry(entry.0).or_insert(0) = 0;
+                    *data
+                        .0
+                        .entry((
+                            instruction.number - (entry.0 .0 - instruction.number),
+                            entry.0 .1,
+                        ))
+                        .or_insert(1) = 1;
+                }
+            }
+        } else {
+            for entry in data.0.clone() {
+                if entry.0 .1 > instruction.number {
+                    // *entry.1 = 0;
+                    *data.0.entry(entry.0).or_insert(0) = 0;
+                    *data
+                        .0
+                        .entry((
+                            entry.0 .0,
+                            instruction.number - (entry.0 .1 - instruction.number),
+                        ))
+                        .or_insert(1) = 1;
+                }
+            }
+        }
+        data.0.iter().filter(|entry| entry.1 == &1).count() as u64
+
+        // Too high: 749
+    }
+    fn two(&self, data: &mut Data) -> u64 {
+        for instruction in &data.1 {
+            if instruction.along_x {
+                for entry in data.0.clone() {
+                    if entry.0 .0 > instruction.number {
+                        if data.0[&entry.0] != 0 {
+                            *data.0.entry(entry.0).or_insert(0) = 0;
+                            *data
+                                .0
+                                .entry((
+                                    (instruction.number as isize
+                                        - (entry.0 .0 as isize - instruction.number as isize))
+                                        .try_into()
+                                        .unwrap(),
+                                    entry.0 .1,
+                                ))
+                                .or_insert(1) = 1;
+                        }
+                    }
+                }
+            } else {
+                for entry in data.0.clone() {
+                    if entry.0 .1 > instruction.number {
+                        if data.0[&entry.0] != 0 {
+                            *data.0.entry(entry.0).or_insert(0) = 0;
+                            *data
+                                .0
+                                .entry((
+                                    entry.0 .0,
+                                    (instruction.number as isize
+                                        - (entry.0 .1 as isize - instruction.number as isize))
+                                        .try_into()
+                                        .unwrap(),
+                                ))
+                                .or_insert(1) = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        for y in 0..6 {
+            for x in 0..40 {
+                if *data.0.entry((x, y)).or_insert(0) == 0 {
+                    print!(" ");
+                } else {
+                    print!("X");
+                }
+            }
+            println!("")
+        }
+
+        0
+    }
 }
-
-
